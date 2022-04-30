04492a8997758e630d8fe685c569cd75dfbdad3c
Dilip <dilippoudel578@gmail.com>
2022-04-30 19:53:40 +0300

    rendered new content

--- catalog/views.py
+++ catalog/views.py
@@ -11,12 +11,16 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(
         status__exact='a').count()
     num_authors = Author.objects.count()
-
+    num_instance_onloan = BookInstance.objects.filter(
+        status__icontains='o').count()
+    num_genre = Genre.objects.count()
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'books_on_loan': num_instance_onloan,
+        'totalGenre': num_genre
     }
 
     return render(request, 'index.html', context=context)

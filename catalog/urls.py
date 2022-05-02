d0e2d9065950359452eeedff4965cebce7a849a0
Dilip <dilippoudel578@gmail.com>
2022-05-02 22:15:30 +0300

    book-detail and book-list landing page created

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='bookslist'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
+
+
+# def get_absolute_url(self):
+#     """Returns the URL to access a particular author instance."""
+#     return reverse('author-details', args=[str(self.id)])
+# fullurel ====> http://127.0.0.8080/catalog/book/1
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 # Create your views here.
 
 
@@ -14,7 +15,7 @@ def index(request):
     num_instance_onloan = BookInstance.objects.filter(
         status__icontains='o').count()
     num_genre = Genre.objects.count()
-    context = {
+    roshan = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
@@ -23,4 +24,27 @@ def index(request):
         'totalGenre': num_genre
     }
 
-    return render(request, 'index.html', context=context)
+    return render(request, 'index.html', context=roshan)
+
+
+#  class based view
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = "book_lists"
+    # Specify your own template name/location
+    template_name = 'book_list.html'
+
+# overriding the method ===>  get_queryset() is a special method in DJango
+    def get_queryset(self):
+        # Get 5 books containing the title war
+        # lists = Book.objects.all()
+        # list = Book.objects.filter(title__icontains='the')
+
+        # return {lists, list}
+
+        return Book.objects.all()
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'

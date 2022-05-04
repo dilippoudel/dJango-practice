c6c45e3546da1cf84704d491550c9fac9ce672c0
Dilip <dilippoudel578@gmail.com>
2022-05-04 23:38:49 +0300

    worked on  styling and the demo of session mechanism

--- catalog/views.py
+++ catalog/views.py
@@ -17,11 +17,15 @@ def index(request):
     num_instance_onloan = BookInstance.objects.filter(
         status__icontains='o').count()
     num_genre = Genre.objects.count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     roshan = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
         'books_on_loan': num_instance_onloan,
         'totalGenre': num_genre
     }
